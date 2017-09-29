c1473567632d6ff11575cf7c08e5f95b72bcf3ee
Miguel Tineo <miketineo@gmail.com>
2017-09-29 15:07:41 +0100

    validation

--- lib/git_magic.rb
+++ lib/git_magic.rb
@@ -1,7 +1,17 @@
 require "git_magic/version"
 
+SHA_PATTERN = /\b[0-9a-f]{5,40}\b/
+
 module GitMagic
-  def commit_progress(sha)
+  def self.commit_progress(sha)
+    raise "need to pass a valid commit sha" unless valid_sha?(sha)
     puts "This would tell where commit #{sha} is in a given moment"
+  end
+
+  private
+
+  def self.valid_sha?(sha)
+    return if sha.empty?
+    sha =~ SHA_PATTERN
   end
 end
